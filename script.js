d4bb97774cf3ff908b2ba11e11c6a9a0178ff2e5
Ryan Flanagan <flanagan.ryan@gmail.com>
2021-01-14 15:52:15 -0500

    obfuscated the script.js code to mask the password

--- script.js
+++ script.js
@@ -1,8 +1,44 @@
-function checkPassword() {
-  if(document.getElementById('password').value == 'ABC123') {
-      return true;
-  } else {
-      alert('Wrong password!');
-      return false;
+var _0x2b50 = [
+  "password",
+  "662140ZFQNXC",
+  "861404fSRMQv",
+  "value",
+  "59945mRYRRE",
+  "1GytSwJ",
+  "325649pFllAX",
+  "574198OGvYEn",
+  "311918djmhhM",
+  "102263snhbga",
+  "Wrong\x20password!",
+];
+var _0x3d60 = function (_0x32361a, _0x40b425) {
+  _0x32361a = _0x32361a - 0x132;
+  var _0x2b50d1 = _0x2b50[_0x32361a];
+  return _0x2b50d1;
+};
+(function (_0x1562cd, _0x3dea78) {
+  var _0x18dce2 = _0x3d60;
+  while (!![]) {
+    try {
+      var _0x3fc6a4 =
+        -parseInt(_0x18dce2(0x137)) +
+        -parseInt(_0x18dce2(0x136)) +
+        -parseInt(_0x18dce2(0x133)) +
+        parseInt(_0x18dce2(0x138)) +
+        parseInt(_0x18dce2(0x13b)) +
+        -parseInt(_0x18dce2(0x135)) * parseInt(_0x18dce2(0x134)) +
+        parseInt(_0x18dce2(0x13c));
+      if (_0x3fc6a4 === _0x3dea78) break;
+      else _0x1562cd["push"](_0x1562cd["shift"]());
+    } catch (_0xbb140e) {
+      _0x1562cd["push"](_0x1562cd["shift"]());
+    }
   }
+})(_0x2b50, 0x56731);
+function checkPassword() {
+  var _0x4285fc = _0x3d60;
+  return document["getElementById"](_0x4285fc(0x13a))[_0x4285fc(0x132)] ==
+    "1020QLRiJFQd"
+    ? !![]
+    : (alert(_0x4285fc(0x139)), ![]);
 }
